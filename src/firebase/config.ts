0ffaef7bc335a5b551f12fe5226b490b6a181eb2
bhavesh pund <bhaveshpund07@gmail.com>
2025-11-04 18:38:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "solarcharge-p2l5t",
+  "appId": "1:781827921948:web:79cd75d7fe5f43d337e7d9",
+  "apiKey": "AIzaSyDR3I2REorQCrqhljRxtJpi5Jch1y6okC4",
+  "authDomain": "solarcharge-p2l5t.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "781827921948"
+};
